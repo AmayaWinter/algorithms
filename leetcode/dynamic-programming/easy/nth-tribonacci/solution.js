2c4d2e85a9f0202d5c6df45da6203bc028f2efd5
Amaya Agha <amaya.agha@gmail.com>
2021-11-28 16:19:07 -0500

    adds solution for LeetCode problem Nth-Tribonnaci

---
+++ leetcode/dynamic-programming/easy/nth-tribonacci/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+//Time: O(n)
+//Space: O(1)
+const tribonacci = function(n) {
+  let nMinusOne = 1;
+  let nMinusTwo = 1;
+  let nMinusThree = 0;
+
+  if(n < 3){
+    return [0,1,1][n];
+  }
+
+  for(let i = 3; i <= n; i++) {
+    let nextNum = nMinusOne + nMinusTwo + nMinusThree;
+    nMinusThree = nMinusTwo;
+    nMinusTwo = nMinusOne;
+    nMinusOne = nextNum
+  }
+
+  return nMinusOne;
+};
